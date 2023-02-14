e807ae83e282341b661eeb67a141311c6e8bfc85
Charbel Salhab <118256379+csalhabb@users.noreply.github.com>
2023-02-14 23:15:10 +0100

    Add files via upload

---
+++ includes/sockets.h
@@ -0,0 +1,14 @@
+/*
+ * Created by : Charbel SALHAB, Andrei MALICEK, Janin MICHEL-MATHIAS
+ * Date : 24/02/2023
+ * "program allowing you to create your own hotel with number of rooms and type of room and service offered.
+ * The rooms can be reserved by customers according to the criteria of the hotel."
+ */
+
+
+typedef struct User {
+
+    char nom[30];
+    char mdp[30];
+
+}User;
